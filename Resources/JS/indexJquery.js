77d2c56114bcccc0e96372cdf027080d4eeedd3c
mquirosa786 <marioquiros96@hotmail.com>
2017-04-08 06:43:21 -0600

    Update indexJquery.js
    borrado codigo innecsario

--- Resources/JS/indexJquery.js
+++ Resources/JS/indexJquery.js
@@ -1,26 +1,10 @@
 //<!-- jquery -->
 $(document).ready(function(){
-  var today = new Date();
-  var ss = 0;
-  function startTime()
-  {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    var t = setTimeout(startTime, 500);
-    ss = s;
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+  
   var indexFondosDerecha =0;
   var listaFondos = ['Resources/Images/volcano.jpg', 'Resources/Images/index2.jpg', 'Resources/Images/index3.jpg',
   'Resources/Images/index4.jpg', 'Resources/Images/index5.jpg'];
-  
+
   var myVar = setInterval(change, 3000);
 
   function change(){
