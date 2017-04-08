67adf8c9808af821c5a4e9321223edb03ff16b29
mquirosa786 <marioquiros96@hotmail.com>
2017-04-08 06:37:25 -0600

    Update indexJquery.js
    auto change

--- Resources/JS/indexJquery.js
+++ Resources/JS/indexJquery.js
@@ -1,8 +1,39 @@
 //<!-- jquery -->
 $(document).ready(function(){
+  var today = new Date();
+  var ss = 0;
+  function startTime()
+  {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var t = setTimeout(startTime, 500);
+    ss = s;
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
   var indexFondosDerecha =0;
   var listaFondos = ['Resources/Images/volcano.jpg', 'Resources/Images/index2.jpg', 'Resources/Images/index3.jpg',
   'Resources/Images/index4.jpg', 'Resources/Images/index5.jpg'];
+  
+  var myVar = setInterval(change, 3000);
+
+  function change(){
+    indexFondosDerecha++;
+    if(indexFondosDerecha == 5){
+      indexFondosDerecha =0;
+    }
+     $("#imageID").fadeOut("slow", function(){
+      $("#imageID").fadeIn("slow");
+      $("#imageID").attr('src', listaFondos[indexFondosDerecha]);
+
+    });
+}
     $("#sliderDerecha").click(function(){
 
 
@@ -28,4 +59,9 @@ $(document).ready(function(){
 
           });
         })
+
+    $("#hide").hover(function(){
+      $("#footer").fadeIn("slow");
+      $(this).hide();
+    })
 });
